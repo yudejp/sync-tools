3693bdbe8d33f2261cc578cda05fb9e9b611d150
yude <i@yude.jp>
2026-05-15 22:04:34 +0900

    exclude much more files

--- sync.ttj1pve1.tail5b1c5.ts.net.sh
+++ sync.ttj1pve1.tail5b1c5.ts.net.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 rsync -avz --partial --delete /mnt/store4/dtv/ root@ttj1pve1.tun.y2e.org:/mnt/store2/dtv/
-rsync -avz --partial --exclude '.git' --delete /mnt/prod/docker/ root@ttj1pve1.tun.y2e.org:/mnt/store1/docker.nrt1sh1/
+rsync -avz --delete --partial \
+	--exclude '.git' \
+	--exclude 'dtv-tuner/config/metadata' \
+	--exclude 'dtv-tuner/epgstation/logs' \
+	--exclude 'dtv-tuner/konomitv/data/thumbnails' \
+	--exclude 'immich/postgres' \
+    --exclude 'jellyfin/config/metadata' \
+    --exclude 'navidrome/data' \
+    --exclude 'paperless/data' \
+    --exclude 'scanopy/db' \
+    --exclude 'scanopy/data' \
+	--delete \
+	/mnt/prod/docker/ \
+	root@ttj1pve1.tun.y2e.org:/mnt/store1/docker.nrt1sh1/
