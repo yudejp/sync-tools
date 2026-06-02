4d8d3ce310d1fbe35125de40ed86669cf1fd7c1e
yude <i@yude.jp>
2026-06-02 23:26:14 +0900

    Update hostname

--- sync.hnd1pve1.tail5b1c5.ts.net.sh
+++ sync.hnd1pve1.tail5b1c5.ts.net.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-rsync -avz --partial --delete /mnt/store1/archives/family/ root@hnd1pve1.tun.y2e.org:/mnt/store1/family/
-rsync -avz --partial --delete /mnt/store3/music/ root@hnd1pve1.tun.y2e.org:/mnt/store1/music/
-rsync -avz --partial --delete /mnt/store3/docs/ root@hnd1pve1.tun.y2e.org:/mnt/store1/docs/
-rsync -avz --partial --delete /mnt/store3/paperless/ root@hnd1pve1.tun.y2e.org:/mnt/store1/paperless/
-rsync -avz --partial --delete /mnt/store3/video/ root@hnd1pve1.tun.y2e.org:/mnt/store1/video/
-rsync -avz --partial --delete /mnt/store3/images/ root@hnd1pve1.tun.y2e.org:/mnt/store1/images/
-rsync -avz --partial --delete /mnt/store3/software/ root@hnd1pve1.tun.y2e.org:/mnt/store1/software/
-rsync -avz --partial --exclude 'immich/backups' --exclude 'immich/encoded-video' --exclude 'immich/profile' --exclude 'immich/thumbs' --exclude 'upload' --delete /mnt/store3/photo/ root@hnd1pve1.tun.y2e.org:/mnt/store1/photo/
+rsync -avz --partial --delete /mnt/store1/archives/family/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/family/
+rsync -avz --partial --delete /mnt/store3/music/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/music/
+rsync -avz --partial --delete /mnt/store3/docs/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/docs/
+rsync -avz --partial --delete /mnt/store3/paperless/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/paperless/
+rsync -avz --partial --delete /mnt/store3/video/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/video/
+rsync -avz --partial --delete /mnt/store3/images/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/images/
+rsync -avz --partial --delete /mnt/store3/software/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/software/
+rsync -avz --partial --exclude 'immich/backups' --exclude 'immich/encoded-video' --exclude 'immich/profile' --exclude 'immich/thumbs' --exclude 'upload' --delete /mnt/store3/photo/ root@hnd1pve1.tail5b1c5.ts.net:/mnt/store1/photo/
--- sync.ttj1pve1.tun.y2e.org.sh
+++ sync.ttj1pve1.tun.y2e.org.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rsync -avz --partial --delete /mnt/store4/dtv/ root@ttj1pve1.tail5b1c5.ts.net:/mnt/store2/dtv/
+rsync -avz --partial --delete /mnt/store4/dtv/ root@ttj1pve1.tun.y2e.org:/mnt/store2/dtv/
 rsync -avz --delete --partial \
 	--exclude '.git' \
 	--exclude 'dtv-tuner/config/metadata' \
@@ -13,4 +13,4 @@ rsync -avz --delete --partial \
     --exclude 'scanopy/db' \
     --exclude 'scanopy/data' \
 	/mnt/prod/docker/ \
-	root@ttj1pve1.tail5b1c5.ts.net:/mnt/store1/docker.nrt1sh1/
+	root@ttj1pve1.tun.y2e.org:/mnt/store1/docker.nrt1sh1/
